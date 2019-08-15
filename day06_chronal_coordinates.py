22d4e24529db67bf4ce288de9e37b2890f57a201
carywang <carywang@carywangdeMacBook-Pro.local>
2019-08-15 21:01:37 +0800

    day6未完成

--- day06_chronal_coordinates.py
+++ day06_chronal_coordinates.py
@@ -48,7 +48,9 @@ In this example, the areas of coordinates A, B, C, and F are infinite - while no
 What is the size of the largest area that isn't infinite?
 """
 
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Set
+
+from collections import Counter
 
 TEST_CASE = """1, 1
 1, 6
@@ -57,25 +59,40 @@ TEST_CASE = """1, 1
 5, 5
 8, 9""".split('\n')
 
-
-class Candidate(Location):
-    x: int
-    y: int
-    area: int
-
-
 class Location(NamedTuple):
     x: int
     y: int
+
+
+class Candidate(NamedTuple):
+    x: int
+    y: int
+    area: int
+    locations: List[Location]
+
+    @classmethod
+    def get_area(self):
+        self.area = len(self.locations)
+        return self.area
 
 
 def get_original_candidates(locations: List[str]):
     candidates: List[Candidate] = []
     for location in locations:
         x, y = [int(coord) for coord in location.split(', ')]
-        candidates.append(Candidate(x, y))
+        candidates.append(Candidate(x, y, 0, []))
     return candidates
 
+def get_max_xy(candidates: List[Candidate]) -> Set[int]:
+    xs: List[int] = []
+    ys: List[int] = []
+    ret = set()
+    for candidate in candidates:
+        xs.append(candidate.x)
+        ys.append(candidate.y)
+    ret.add(max(xs))
+    ret.add(max(ys))
+    return ret
 
 def get_all_locations(max_x: int, max_y: int) -> List[Location]:
     locations: List[Location] = []
@@ -84,12 +101,25 @@ def get_all_locations(max_x: int, max_y: int) -> List[Location]:
             locations.append(Location(x, y))
     return locations     
 
-def calculate_manhattan_distance(l1: Location, l2: Location) -> int:
+def calculate_manhattan_distance(l1: Location, l2: Candidate) -> int:
     return abs(l1.x - l2.x) + abs(l1.y - l2.y)
 
-def exclude_candidates_with_infinite_area(candidates: List[Candidate]) -> List[Candidate]:
-    filtered = candidates
+def set_locations_to_candidate(locations: List[Location], candidates: List[Candidate]) -> int:
+    for location in locations:
+        distances: Counter = Counter()
+        for candidate in candidates:
+            distances[candidate] = calculate_manhattan_distance(location, candidate)
+        biggest_two = distances.most_common(2)
+        if biggest_two[0] > biggest_two[1]:
+            print(biggest_two)
+            # biggest_two[0][0].locations.append(location)
+    areas = []
     for candidate in candidates:
-        edge_point1 = Location(0, candidate.y)
-        edge_point2 = Location(candidate.x, 0)
-        filter1 = calculate_manhattan_distance()
+        areas.append(candidate.get_area())
+    return max(areas)
+
+candidates = get_original_candidates(TEST_CASE)
+max_x, max_y = get_max_xy(candidates)
+locations = get_all_locations(max_x, max_y)
+
+print(set_locations_to_candidate(locations, candidates))

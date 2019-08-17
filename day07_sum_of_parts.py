b2b5ffda03e54a512c4d131ddf0f376ba3c7288e
carywang <carywang@carywangdeMacBook-Pro.local>
2019-08-17 11:56:49 +0800

    day07 未完成

--- day07_sum_of_parts.py
+++ day07_sum_of_parts.py
@@ -33,7 +33,7 @@ So, in this example, the correct order is CABDFE.
 In what order should the steps in your instructions be completed?
 """
 
-from typing import List, Dict, Set, NamedTuple
+from typing import List, Dict, Set, Tuple
 import re
 
 
@@ -41,11 +41,13 @@ class Step:
     name: str
     prev_steps: List[str]
     next_steps: List[str]
+    finish_time: int
 
     def __init__(self, name):
         self.name = name
         self.prev_steps = []
         self.next_steps = []
+        finish_time = 99999
 
 
 TEST_CASE = """Step C must be finished before step A can begin.
@@ -81,20 +83,12 @@ def get_steps(instructions: List[str]):
     return steps
 
 
-def get_first_step(steps: Dict[str, Step]) -> Dict[str, Step]:
-    all_steps = list(steps.values())
-    first_steps = {}
-    for step in all_steps:
-        if step.prev_steps == []:
-            first_steps[step.name] = step
-    return first_steps
-
-
 def find_available_step(steps: Dict[str, Step], finished_steps: str):
-    sorted_steps = sorted(steps.items(), key = lambda x: x[0])
+    sorted_steps = sorted(steps.items(), key=lambda x: x[0])
     for i, sorted_step in sorted_steps:
         if all([prev_step in finished_steps for prev_step in sorted_step.prev_steps]):
             return sorted_step
+
 
 def process(steps: Dict[str, Step]):
     order = ''
@@ -103,10 +97,103 @@ def process(steps: Dict[str, Step]):
         order += candidate.name
         del steps[candidate.name]
     return order
-    
+
 
 # print(process(get_steps(TEST_CASE)))
 
-with open('models/day07.txt') as f:
-    instructions = [line.strip() for line in f.readlines()]
-    print(process(get_steps(instructions)))
+# with open('models/day07.txt') as f:
+#     instructions = [line.strip() for line in f.readlines()]
+#     print(process(get_steps(instructions)))
+
+
+"""
+As you're about to begin construction, four of the Elves offer to help. "The sun will set soon; it'll go faster if we work together." Now, you need to account for multiple people working on steps simultaneously. If multiple steps are available, workers should still begin them in alphabetical order.
+
+Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on. So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.
+
+To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers) and that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds). Then, using the same instructions as above, this is how each second would be spent:
+
+Second   Worker 1   Worker 2   Done
+   0        C          .        
+   1        C          .        
+   2        C          .        
+   3        A          F       C
+   4        B          F       CA
+   5        B          F       CA
+   6        D          F       CAB
+   7        D          F       CAB
+   8        D          F       CAB
+   9        D          .       CABF
+  10        E          .       CABFD
+  11        E          .       CABFD
+  12        E          .       CABFD
+  13        E          .       CABFD
+  14        E          .       CABFD
+  15        .          .       CABFDE
+Each row represents one second of time. The Second column identifies how many seconds have passed as of the beginning of that second. Each worker column shows the step that worker is currently doing (or . if they are idle). The Done column shows completed steps.
+
+Note that the order of the steps has changed; this is because steps now take time to finish and multiple workers can begin multiple steps simultaneously.
+
+In this example, it would take 15 seconds for two workers to complete these steps.
+
+With 5 workers and the 60+ second step durations described above, how long will it take to complete all of the steps?
+"""
+
+# 检查有几个空闲工人
+def check_worker_availability(working_steps: Dict[str, Step], current_time: int, finished_steps: Set[str]) -> Tuple[int, Dict[str, Step], Set[str]]:
+    WORKER_CAPACITY = 2
+    finished: Set[str] = set()
+    print('!!!!!!', working_steps)
+    print('!!!!!!', working_steps.items())
+
+    for i, working_step in working_steps.items():
+        if working_step.finish_time <= current_time:
+            print('adding, ', working_step.__dict__)
+            finished.add(working_step.name)
+    print('!!!!!!!!!!!!!!!!!', finished)
+    for finished_work in finished:
+        del working_steps[finished_work]
+    finished_steps.update(finished)
+    del finished
+    return WORKER_CAPACITY - len(working_steps), working_steps, finished_steps
+
+# 从unfinished_steps里找出不多余空闲数量的工作给他们
+def disptach_work(unfinished_steps: Dict[str, Step], finished_steps: Set[str], working_steps: Dict[str, Step], available_workers: int, current_time: int):
+    letters = {chr(i): i - 64 for i in range(65, 91)}
+    sorted_steps = sorted(unfinished_steps.items(), key=lambda x: x[0])
+    dispatchable_works: List[str] = []
+    for i, sorted_step in sorted_steps:
+        if all([prev_step in finished_steps for prev_step in sorted_step.prev_steps]):
+            dispatchable_works.append(sorted_step.name)
+            dispatchable_works.sort()
+    print('current_time, dispatchable_works', current_time, dispatchable_works)
+    for j in range(min(available_workers, len(dispatchable_works))):
+        step = find_available_step(unfinished_steps, ''.join(finished_steps))
+        step.finish_time = current_time + letters[step.name]
+        working_steps[dispatchable_works[j]] = step
+    for dispatchable_work in dispatchable_works:
+        del unfinished_steps[dispatchable_work]
+    
+    return unfinished_steps, finished_steps, working_steps
+
+def work(steps: Dict[str, Step]):
+    current_time = 0
+    unfinished_steps = steps
+    finished_steps: Set[str] = set()
+    working_steps: Dict[str, Step] = {}
+
+    while len(unfinished_steps) is not 0:
+        print('before: ', current_time, working_steps, finished_steps)
+        available_workers, working_steps, finished_steps = check_worker_availability(working_steps, current_time, finished_steps)
+
+        unfinished_steps, finished_steps, working_steps = disptach_work(unfinished_steps, finished_steps, working_steps, available_workers, current_time)
+        print('after: ', unfinished_steps, finished_steps, working_steps)
+
+        current_time += 1
+    return current_time
+
+print(work(get_steps(TEST_CASE)))
+# print(range(1))
+# for i in range(2):
+#     print(i)
+
